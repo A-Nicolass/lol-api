61f91eb6fb0318d7d769796c2c40cafeaa396d95
Alexandre Nicolas <alexandre.workline@gmail.com>
2023-03-12 00:42:30 +0100

    first commit

---
+++ src/components/test.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <label for="summoner-name">Summoner name:</label>
+    <input type="text" id="summoner-name" v-model="summonerName">
+    <button @click="fetchSummonerData">Get data</button>
+
+    <div v-if="summoner">
+      <h2>{{ summoner.name }} ({{ summoner.summonerLevel }})</h2>
+      <h3>Match participants:</h3>
+      <ul>
+        <li v-for="(participant, index) in participants" :key="index">
+          <div>
+            <img :src="participant.championImg" :alt="participant.championName" width="50">
+            {{ participant.summonerName }} - {{ participant.championName }}
+          </div>
+          <!-- <div>
+            <img :src="getSpellImageUrl(participant.summoner1Id)" :alt="participant.summoner1Name" width="20">
+            <img :src="getSpellImageUrl(participant.summoner2Id)" :alt="participant.summoner2Name" width="20">
+          </div> -->
+
+        </li>
+      </ul>
+    </div>
+
+    <div v-if="loading">
+      Loading...
+    </div>
+  </div>
+</template>
+
+<script>
+import { getSummoner, getGame, getAllChampion } from "../services/helper.js";
+
+export default {
+  data() {
+    return {
+      summonerName: "",
+      summoner: null,
+      participants: [],
+      loading: false,
+      apiKey: "RGAPI-bfded700-d3ad-4718-85c6-f68be07d3821", // Replace with your actual API key
+      dataDragonVersion: "12.6.1", // Replace with the current Data Dragon version
+      championData: {}, // Add this property
+      
+    };
+  },
+  async created() {
+  const championData = await getAllChampion();
+  console.log(championData)
+  this.championData = championData;
+},
+
+    methods: {
+      async fetchSummonerData() {
+  this.loading = true;
+
+  try {
+    const summoner = await getSummoner(this.summonerName, this.apiKey);
+    const game = await getGame(summoner.id, this.apiKey);
+    console.log(summoner.id)
+
+    // Replace champion ID with champion name
+    const participants = game.map((participant) => {
+      return {
+        ...participant,
+        championName: this.championData[participant.championId].name,
+        championImg: this.championData[participant.championId].img,
+      };
+    });
+    
+    this.summoner = summoner;
+    this.participants = participants;
+    
+
+    console.log(participants)
+  } catch (error) {
+    console.error(error);
+    alert("Error fetching data!");
+  }
+
+  this.loading = false;
+},
+
+    // getChampionImageUrl(championId) {
+    //   return `http://ddragon.leagueoflegends.com/cdn/${this.dataDragonVersion}/img/champion/${championId}.png`;
+    // },
+    getSpellImageUrl(spellId) {
+      return `http://ddragon.leagueoflegends.com/cdn/${this.dataDragonVersion}/img/spell/${spellId}.png`;
+    }
+
+  }
+};
+</script>
---
+++ src/services/helper.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+export async function getSummoner(summonerName, apiKey) {
+  const resp = await axios.get(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${apiKey}`);
+  return resp.data;
+}
+
+export async function getGame(encryptedSummonerId, apiKey) {
+  const resp = await axios.get(
+    `https://euw1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/${encryptedSummonerId}?api_key=${apiKey}`
+  );
+  return resp.data.participants;
+}
+
+export async function getAllChampion() {
+    const resp = await axios.get(
+      `http://ddragon.leagueoflegends.com/cdn/11.6.1/data/fr_FR/champion.json`
+    );
+    const championData = resp.data.data;
+  
+    // Create an object with champion names indexed by champion ID
+    const champions = {};
+    for (const [key, value] of Object.entries(championData)) {
+      champions[value.key] = {
+        name: value.name,
+        img: `http://ddragon.leagueoflegends.com/cdn/11.6.1/img/champion/${key}.png`,
+      }
+     
+    }
+  
+    return champions;
+  }
+  
+// export async function getChampionImg(championName) {
+//     const resp = await axios.get(
+//       `http://ddragon.leagueoflegends.com/cdn/11.6.1/img/champion/${championName}.png`
+//     );
+//     return resp;
+//   }
